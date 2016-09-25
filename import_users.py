48c7070d6f83b610fd73090a6287df4840f900ee
Jerry Tan <jerrytansk@gmail.com>
2016-09-25 23:23:37 +0800

    periodic inserts

--- import_users.py
+++ import_users.py
@@ -9,7 +9,7 @@ MYSQL_HOST     = '104.196.149.230'
 # PATH_TO_DATA   = "/Volumes/Mac/data/links1.txt"
 # PATH_TO_DATA = "gs://peerrank-141304.appspot.com/data/links-anon.txt"
 # PATH_TO_DATA = "/tmp/links%s.txt"
-PATH_TO_DATA   = "/tmp/links-anon.txt"
+PATH_TO_DATA   = "/tmp/links1.txt"
 PER_PAGE       = 100000
 NO_PROCESS     = 10
 
@@ -19,23 +19,27 @@ def main(hostname=None):
 	cursor = cnx.cursor()
 	f = open(PATH_TO_DATA, 'r')
 	data = []
+	line_count = 0
 	print("Reading file...")
 	for line in f:
 		(follower, followee) = line.split(' ')
 		data.append((int(follower.strip('\n')), int(followee.strip('\n'))))
-	temp = set(map(lambda x: x[1], data))
-	total = len(temp)
-	count = 0
-	print("Entering data...")
-	for user in temp:
-		try:
-			cursor.execute("INSERT IGNORE INTO test.temp (id) VALUES(%s)" % user)
-			cursor.execute("INSERT IGNORE INTO test.new_temp (user_id) VALUES(%s)" % user)
-			count += 1
-			if (count / 1.0 * total * 100) % 1 < 0.05:
-				print(str(count / 1.0 * total * 100) + " completed")
-		except Exception as e:
-			print e
+		line_count += 1
+		if line_count % 100000 == 0:
+			temp  = set(map(lambda x: x[1], data))
+			total = len(temp)
+			count = 0
+			print("Entering data...")
+			for user in temp:
+				try:
+					cursor.execute("INSERT IGNORE INTO test.temp (id) VALUES(%s)" % user)
+					cursor.execute("INSERT IGNORE INTO test.new_temp (user_id) VALUES(%s)" % user)
+					count += 1
+					if (count / 1.0 * total * 100) % 1 < 0.05:
+						print(str(count / 1.0 * total * 100) + " completed")
+				except Exception as e:
+					print e
+			data = []
 
 if __name__ == "__main__":
 	if sys.argv[1] is not None:
