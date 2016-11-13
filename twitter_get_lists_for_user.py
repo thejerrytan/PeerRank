07ecccc368412ea0d6689be00d80caf114513f3a
Jerry Tan <jerrytansk@gmail.com>
2016-11-13 18:36:57 +0800

    charset and collation for mysql connection pool

--- twitter_get_lists_for_user.py
+++ twitter_get_lists_for_user.py
@@ -13,7 +13,7 @@ NUM_USERS         = 500000
 NO_THREADS        = 5
 USERS_PER_PROCESS = math.ceil(NUM_USERS / NO_THREADS)
 SO_FAR            = int(open('twitter_get_lists_for_user.txt', 'r').readline())
-cnx               = mysql.connector.connect(pool_name="mypool", pool_size=6, user=MYSQL_USER, password=MYSQL_PW, host=MYSQL_HOST, database='test', connection_timeout=3600)
+cnx               = mysql.connector.connect(pool_name="mypool", pool_size=6, user=MYSQL_USER, password=MYSQL_PW, host=MYSQL_HOST, database='test', charset='utf8mb4', collation='utf8mb4_general_ci', connection_timeout=3600)
 cursor            = cnx.cursor()
 qlock             = threading.Lock()
 
